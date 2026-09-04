32253ca79122dfa32189b3f26d4e4b9794310051
TJmonat <ST10481840@rcconnect.edu.za>
2026-09-04 18:43:13 +0200

    Add files via upload
    Section C: Inserting Data

---
+++ SQLQueryr2.sql
@@ -0,0 +1,41 @@
+Insert Into Participant
+Values
+('P001','Tshegofatso','Modise',20,'tshego.m@icloud.com'),
+('P002','Lesedi','Nkuna',21,'lesediNkuna@gamil.com');
+
+
+Insert Into Event_Category
+Values
+('CAT001','Walking','Community Walk'),
+('CAT002','Running','Park Runs'),
+('CAT003','Cycling','Charity Cycling');
+
+
+Insert Into Event_Organizer
+Values
+('Org001','Gianl Intanio',45,'Intanio.fifa.co.za'),
+('Org002','Mattias Granfstorm',55,'granfstorm@gmail.com');
+
+Insert Into Venue
+Values
+('VEN001','Moses Mbida Stadium','Durban'),
+('VEN002','DHL Stadium','Cape town'),
+('VEN003','Eikla Stadium','Soweto');
+
+
+Insert Into RaceEvent
+Values
+('EV001','Capetown','2026-10-10'),
+('EV002','Durban','2026-11-09'),
+('EV003','Soweto','2026-05-04');
+
+
+Insert Into Registration
+Values
+('REG001','Registered','2026-10-10','09:00:30'),
+('REG002','Pending','2026-11-09','11:30:20');
+
+Insert Into Results
+Values
+('RES001','Gold'),
+('RES002','Silver');
